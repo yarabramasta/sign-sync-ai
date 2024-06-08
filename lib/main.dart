b9c526d3587ebc3d3dd747c82c4ec910f6429447
yarabramasta <yarabram111@gmail.com>
2024-06-08 12:53:48 +0700

    chore: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 import 'theme/theme.dart';
 import 'theme/util.dart';
 
-void main() {
+void main() async {
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
